8051577de4a10421ea05932819133bf4d4a95127
Jan Diehm <jadiehm@gmail.com>
2019-06-28 15:18:43 -0500

    first 2 steps of scrape

---
+++ process/s1_downloadCallHTML.js
@@ -0,0 +1,37 @@
+//Require basically means load this node package so that I can access it later
+const fs = require('fs');
+const d3 = require('d3');
+const request = require('request');
+const cheerio = require('cheerio');
+
+//This is the file path for where I will save the files
+const OUT_PATH = './output/callHTML/'
+//Base URL of the webpage to target
+const BASE_URL = 'https://www.fool.com/earnings-call-transcripts/?page='
+//Array of all the page numbers to add to the BASE_URL
+const CALL_NUMS = [1, 2, 3]
+
+//Function that actually pulls the page - it takes a number
+async function getCallHTML(number) {
+	//BASE_URL with the number from above added to the end
+	const NUM_URL = `${BASE_URL}${number}`
+	//This is the same as above, just different syntax
+	//const NUM_URL = BASE_URL + number
+	//Basic promise syntax
+	return new Promise((resolve, reject) => {
+		//Contact the URL (body is the html we're looking for)
+		request(NUM_URL, (err, response, body) => {
+			//Write and save the body or HTML out to out OUT_PATH
+			fs.writeFileSync(`${OUT_PATH}/page-${number}.html`, body)
+		})
+	})
+}
+
+//Initial function (THIS IS THE ONLY FUNCTION BEING CALLED - PLACE THINGS INSIDE OF ME!)
+function init() {
+	//Maps over each value in the CALL_NUMS array and calls the getCallHTML once for each value
+	CALL_NUMS.map(getCallHTML);
+}
+
+//RUN THE INIT FUNCTION!
+init();
---
+++ process/s2_getCallLinks.js
@@ -0,0 +1,60 @@
+//Require basically means load this node package so that I can access it later
+const fs = require('fs');
+const d3 = require('d3');
+const request = require('request');
+const cheerio = require('cheerio');
+
+//Empty array to house the data that we will collect
+const callLinks = [];
+
+//The files I need to pull in
+const IN_PATH = './output/callHTML/'
+
+//This is the file path for where I will save the files
+const OUT_PATH = './output/'
+
+//Base url that we build upon
+const BASE_URL = 'https://www.fool.com'
+
+//Array of all the page numbers to add to the BASE_URL
+const CALL_NUMS = [1, 2, 3]
+
+//function to find the things we need in the html
+function getCallLinks(number){
+	//Loads the HTML file
+	const file = fs.readFileSync(`${IN_PATH}page-${number}.html`, 'utf-8');
+	//Allows us to access the HTML file
+	const $ = cheerio.load(file)
+
+	//Find the element in the HTML we need
+	$('.list-content')
+		.each((i, el) => {
+			const article = $(el).find('article')
+			article.each((i, el) => {
+				const title = $(el).find('h4')
+				const linkRef = title.find('a')
+				const link = linkRef.attr('href')
+				//Combines the urls
+				const linkCombined = `${BASE_URL}${link}`
+				//If the link exists then push that link to the callLinks array
+				if (linkCombined) callLinks.push({ linkCombined })
+			})
+		})
+	return callLinks;
+}
+
+function init() {
+	CALL_NUMS.map(getCallLinks);
+
+	//Combine the callLinks array into one big data chuck
+	const allLinks = [].concat(...callLinks).map(link => ({
+		...link
+	}));
+
+	//Format that data chunk into a csv
+	const csvLinks = d3.csvFormat(allLinks);
+	//Save that csv
+	fs.writeFileSync(`${OUT_PATH}links.csv`, csvLinks)
+}
+
+init();
